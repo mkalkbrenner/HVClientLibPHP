91582ed05d01cfce8bc81cbf7d3e01b26d057fa9
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2013-03-18 22:05:49 +0100

    not return a string value in HVClient_AbstractXmlEntity::__get

--- HVClient/HVClient_AbstractXmlEntity.php
+++ HVClient/HVClient_AbstractXmlEntity.php
@@ -29,9 +29,7 @@ abstract class HVClient_AbstractXmlEntity {
     else {
       $name = str_replace('_', '-', $name);
       if (isset($this->payload->$name)) {
-        // typecast to string returns the value of a node instead of
-        // a reference to the internal simple xml object
-        return (string) $this->payload->$name;
+        return $this->payload->$name;
       }
     }
 
